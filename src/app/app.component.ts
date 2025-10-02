6feb8737b8a38fbc65b3f6b18ac5f220daa27893
Kaaisel <santistebangas@gmail.com>
2025-10-02 09:08:31 +0200

    Mejor subido el home, login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Home } from './home/home';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,Home],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
-import { Home } from './home/home';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
 
 export const routes: Routes = [
 
 
-    {path:'',component:Home}
+    {path:'',component:HomeComponent},
+    {path:'login',component:LoginComponent}
 
 ];

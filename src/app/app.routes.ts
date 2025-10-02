a342d34a2578a0fd3602c992a266887a8a53cf5a
Kaaisel <santistebangas@gmail.com>
2025-10-02 11:02:00 +0200

    Ajuste de carpetas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './login/registro/registro.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { DmComponent } from './dm/dm.component';
 
 
 export const routes: Routes = [
 
 
     {path:'',component:HomeComponent},
-    {path:'login',component:LoginComponent}
+    {path:'login',component:LoginComponent},
+    {path:'registro',component:RegistroComponent},
+    {path:'usuario',component:UsuarioComponent},
+    {path:'dm',component:DmComponent}
 
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  constructor(private router: Router) { }
+
+  iniciarSesion() {
+        this.router.navigate(['usuario'])
+  }
+
 
 }
